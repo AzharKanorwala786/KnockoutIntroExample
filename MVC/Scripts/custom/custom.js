ed5d821642e545ac87abb2782611a1cc54b217e0
Azhar Kanorwala <Azhar13754@Mastek.Com>
2017-06-23 09:48:31 +0530

    Base

---
+++ MVC/Scripts/custom/custom.js
@@ -0,0 +1,47 @@
+﻿var demo = demo || {
+
+    Init: function () {
+
+        //var model = function () {
+
+        //    var self = this;
+        //    self.valone = ko.observable();
+        //    self.valtwo = ko.observable();
+
+        //    item = function (valueone, valuetwo) {
+        //        var self = this;
+        //        self.valone = ko.observable(valueone);
+        //        self.valtwo = ko.observable(valuetwo);
+        //    }
+
+        //    self.addItem = function () {
+        //        var self = this;
+        //        self.sum = 
+
+        //    }
+        //}
+
+
+        var Viewmodel = function () {
+            var self = this;
+            self.name = ko.observable();
+            self.meal = ko.observable();
+            self.price = ko.observable();
+            self.itemArray = ko.observableArray();
+
+            item = function(Name,Meal,Price) {
+                var self = this;
+                self.name = ko.observable(Name);
+                self.meal = ko.observable(Meal);
+                self.price = ko.observable(Price);
+            }
+
+            self.addItem = function () {
+                self.itemArray.push(new item(self.name(), self.meal(), self.price()));
+                self.name('');
+                self.meal('');
+            };
+        }
+        ko.applyBindings(new Viewmodel());
+    },
+}
---
+++ MVC/Scripts/custom/customs.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+
+    var Model = function () {
+        var self = this;
+
+        self.name = ko.observable();
+        self.meal = ko.observable();
+        self.price = ko.observable();
+        self.itemArray = ko.observableArray();
+
+        self.addItem = function () {
+            self.itemArray.push(new item(self.name(), self.meal(), self.price()));
+            self.name('');
+            self.meal('');
+        };
+    };
+
+    function item(Name, Meal, Price) {
+        var self = this;
+        self.name = ko.observable(Name);
+        self.meal = ko.observable(Meal);
+        self.price = ko.observable(Price);
+    }
+
+    ko.applyBindings(new Model());
+
+});
